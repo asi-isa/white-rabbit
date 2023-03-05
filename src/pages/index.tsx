489860a6cde9017ed24fe981c6f062324f3fd514
asi-isa <ali.inceoglu@protonmail.com>
2023-03-05 21:15:49 +0100

    display msg on ui, scaffold

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { listen } from "@tauri-apps/api/event";
 
 import MessageInput from "../components/MessageInput";
 
 function App() {
-  useEffect(() => {
-    listenAsync();
-  }, []);
+  const [msgs, setMsgs] = useState<string[]>([]);
 
-  async function listenAsync() {
-    await listen("rcv", (e) => {
+  useEffect(() => {
+    const unlisten = listen("rcv", (e) => {
       console.log(e);
       console.log(e.payload);
+      const author = Math.random() > 0.5 ? "green" : "blue";
+
+      setMsgs((currentMsgs) => {
+        const newMsgs = `${author}: ${e.payload}`;
+        console.log("newMsgs", newMsgs);
+
+        return [...currentMsgs, `${author}: ${(e.payload as any).message}`];
+      });
     });
-  }
+
+    return () => {
+      unlisten.then((f) => f());
+    };
+  }, []);
 
   return (
     <div className="h-screen flex flex-col justify-end gap-4 p-3">
       <div>
-        <p>green: Hello</p>
-        <p>green: world!</p>
-        <p>blue: Si!</p>
+        {msgs.map((msg, i) => {
+          return <p key={i}>{msg}</p>;
+        })}
       </div>
 
       <MessageInput />
